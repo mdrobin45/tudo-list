508f3a29873758872f97d39337ea138c51475576
Md Robin Rana <robin4500bd@gmail.com>
2021-09-11 22:15:48 +0600

    d

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ document.getElementById('addBtn').addEventListener('click',saveToLocalStorage);
 // Save task to local storage
 function saveToLocalStorage()
 {
-    
     const inputValue = document.getElementById('todoInput').value;
     const id = Math.floor(Math.random() * 1000);
     const task = {id,inputValue};
@@ -18,6 +17,8 @@ function saveToLocalStorage()
     displayTask();
 }
 displayTask()
+
+
 // Display task
 function displayTask()
 {
@@ -25,6 +26,16 @@ function displayTask()
     displayTasks.innerHTML = '';
     const tasks = JSON.parse(localStorage.getItem('tasks'));
     tasks.forEach(element => {
-        displayTasks.innerHTML += `<li class="py-3 rounded-md text-lg text-left px-3 my-2 w-6/12 m-auto bg-purple-200">${element.inputValue}</li>`;
+        displayTasks.innerHTML += `<div class="grid grid-cols-2 py-3 rounded-md text-lg text-left px-3 my-2 w-6/12 m-auto bg-purple-200"><li>${element.inputValue}</li><div><img onclick="deleteTask(${element.id})" class="w-5 cursor-pointer float-right" src="icon/close.png"/></div></div>`;
     });
+}
+
+
+// Close Task
+function deleteTask(id)
+{
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    const remainTask = Object.values(tasks).filter(item => item.id != id);
+    localStorage.setItem('tasks', JSON.stringify(remainTask));
+    displayTask();
 }
